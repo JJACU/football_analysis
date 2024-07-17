1613efe3339e0be120092525482875ff8c3a1a90
Jacek <jacukopec@gmail,com>
2024-07-17 16:34:38 +0200

    player with most goals in dataset

--- project.py
+++ project.py
@@ -5,3 +5,10 @@ df = pd.read_csv('C:\pythonML\events.csv')
 
 new_df = df[df['text'].str.contains(r'Goal', case=False, na=False )]
 print(new_df.count())
+
+goal_counts = new_df['player'].value_counts()
+
+top_player =  goal_counts.idxmax()
+top_count = goal_counts.max()
+
+print(f'Graczem z największą iloscia goli jest {top_player} z iloscia goli {top_count}')
