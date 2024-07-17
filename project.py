d5abb75f992d9d7ec99e212fc06679b69092d786
Jacek <jacukopec@gmail,com>
2024-07-17 23:01:34 +0200

    all goals with any assist

--- project.py
+++ project.py
@@ -3,12 +3,5 @@ import re
 
 df = pd.read_csv('C:\pythonML\events.csv')
 
-new_df = df[df['text'].str.contains(r'Goal', case=False, na=False )]
-
-
-goal_counts = new_df['player'].value_counts()
-
-top_player =  goal_counts.idxmax()
-top_count = goal_counts.max()
-
-print(f'Graczem z największą iloscia goli jest {top_player} z iloscia goli {top_count}')
+goal_df = df[df['text'].str.contains(r'Goal!') & df['assist_method'] != 0]
+print(goal_df.tail())
