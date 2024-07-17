8b507a4beb487c27312fe5baf073a320a93d2b3e
Jacek <jacukopec@gmail,com>
2024-07-17 23:24:50 +0200

    all goals scored from centre of the box with right foot

--- project.py
+++ project.py
@@ -3,5 +3,12 @@ import re
 
 df = pd.read_csv('C:\pythonML\events.csv')
 
-goal_df = df[df['text'].str.contains(r'Goal!') & df['assist_method'] != 0]
-print(goal_df.tail())
+
+
+# Filtracja danych
+goal_df = df[df['text'].str.contains(r'Goal')]
+new_df = goal_df.query('bodypart == 1.0 & location == 3.0')
+
+# Wyświetlenie wyniku
+print(new_df[['text', 'event_type', 'event_team', 'player', 'player2', 'location', 'bodypart', 'assist_method']].head(10))
+
