e1dae197c016e939d876ca15f018f38e7ab263bb
Jacek <jacukopec@gmail,com>
2024-08-12 20:47:31 +0200

    second option

--- project.py
+++ project.py
@@ -4,10 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns  # visualization tool
 
 data = pd.read_csv('C:\\pythonML\\pokemon_data.csv')
-data.Speed.plot(kind = 'line', color = 'g',label = 'Speed',linewidth=1,alpha = 0.5,grid = True,linestyle = ':')
-data.Defense.plot(color = 'r',label = 'Defense',linewidth=1, alpha = 0.5,grid = True,linestyle = '-.')
-plt.legend(loc='upper right')     # legend = puts label into plot
-plt.xlabel('x axis')              # label = name of label
-plt.ylabel('y axis')
-plt.title('Line Plot')            # title = title of plot
+data.plot(kind='scatter', x='Attack', y='Defense',alpha = 0.5,color = 'red')
+plt.xlabel('Attack')              # label = name of label
+plt.ylabel('Defence')
+plt.title('Attack Defense Scatter Plot')   
 plt.show()
